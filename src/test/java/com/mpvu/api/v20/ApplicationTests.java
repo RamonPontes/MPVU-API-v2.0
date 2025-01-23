9871a575ec97db8d0555d1621a4322c83ca61752
RamonPontes <rgdpontes@yahoo.com>
2025-01-23 12:46:56 -0300

    Init commit

---
+++ src/test/java/com/mpvu/api/v20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpvu.api.v20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
