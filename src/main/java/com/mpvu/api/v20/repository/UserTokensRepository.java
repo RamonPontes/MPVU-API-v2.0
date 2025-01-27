cb3bb89bbf478ad7eab252950cd43948cd993d56
RamonPontes <rgdpontes@yahoo.com>
2025-01-26 21:31:20 -0300

    feat: add login in google callback

--- src/main/java/com/mpvu/api/v20/repository/UserTokensRepository.java
+++ src/main/java/com/mpvu/api/v20/repository/UserTokensRepository.java
@@ -3,5 +3,8 @@ package com.mpvu.api.v20.repository;
 import com.mpvu.api.v20.model.UserTokens;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserTokensRepository extends JpaRepository<UserTokens, Integer> {
+    Optional<UserTokens> findByToken(String token);
 }
--- src/main/java/com/mpvu/api/v20/service/oauth2/google/GoogleCallbackService.java
+++ src/main/java/com/mpvu/api/v20/service/oauth2/google/GoogleCallbackService.java
@@ -2,7 +2,11 @@ package com.mpvu.api.v20.service.oauth2.google;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mpvu.api.v20.model.ConfirmPasswordToken;
+import com.mpvu.api.v20.model.User;
+import com.mpvu.api.v20.model.UserTokens;
 import com.mpvu.api.v20.repository.ConfirmPasswordTokenRepository;
+import com.mpvu.api.v20.repository.UserRepository;
+import com.mpvu.api.v20.repository.UserTokensRepository;
 import com.mpvu.api.v20.service.platforms.GoogleTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +18,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class GoogleCallbackService {
@@ -26,7 +32,13 @@ public class GoogleCallbackService {
     @Autowired
     ConfirmPasswordTokenRepository confirmPasswordTokenRepository;
 
-    public String callback(String code) throws IOException, InterruptedException {
+    @Autowired
+    UserTokensRepository userTokensRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Object callback(String code) throws IOException, InterruptedException {
         if (code == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Code are required");
         }
@@ -39,24 +51,43 @@ public class GoogleCallbackService {
 
         String refreshToken = jsonNode.get("refresh_token").toString().replace("\"", "");
 
-        var now = Instant.now();
-        var expiresIn = 60 * 10L; // 10m
+        Optional<UserTokens> userTokens = userTokensRepository.findByToken(refreshToken);
 
-        var claims = JwtClaimsSet.builder()
-                .issuer("MpvuAPIv2.0")
-                .subject(refreshToken)
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiresIn))
-                .build();
+        if (userTokens.isPresent()) {
+            User user = userRepository.findById(userTokens.get().getUserId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid user"));
+
+            var now = Instant.now();
+            var expiresIn = 60 * 60 * 24 * 30L; // 30d
+
+            var claims = JwtClaimsSet.builder()
+                    .issuer("MpvuAPIv2.0")
+                    .subject(user.getUserId().toString())
+                    .issuedAt(now)
+                    .expiresAt(now.plusSeconds(expiresIn))
+                    .build();
+
+            return Map.of("type", "login", "token", jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
+        } else {
+            var now = Instant.now();
+            var expiresIn = 60 * 10L; // 10m
+
+            var claims = JwtClaimsSet.builder()
+                    .issuer("MpvuAPIv2.0")
+                    .subject(refreshToken)
+                    .issuedAt(now)
+                    .expiresAt(now.plusSeconds(expiresIn))
+                    .build();
 
 
-        ConfirmPasswordToken confirmPasswordToken = new ConfirmPasswordToken();
-        confirmPasswordToken.setToken(refreshToken);
-        confirmPasswordToken.setCreatedAt(now);
-        confirmPasswordToken.setExpiresAt(now.plusSeconds(expiresIn));
+            ConfirmPasswordToken confirmPasswordToken = new ConfirmPasswordToken();
+            confirmPasswordToken.setToken(refreshToken);
+            confirmPasswordToken.setCreatedAt(now);
+            confirmPasswordToken.setExpiresAt(now.plusSeconds(expiresIn));
 
-        confirmPasswordTokenRepository.save(confirmPasswordToken);
+            confirmPasswordTokenRepository.save(confirmPasswordToken);
 
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+            return Map.of("type", "login", "token", jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue());
+        }
     }
 }
